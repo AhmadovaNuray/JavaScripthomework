58c21e259fe9f34e1b4eae82d19d34c707cdded6
nryehmdva <nurayahmadova@Nurays-MacBook-Pro.local>
2023-12-07 14:32:49 +0400

    file added

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 
-const characters = prompt(`please enter a new password`)
+const password = prompt(`please enter a new password`);
 
 if (password.lenght >= 6 ){ 
 
     alert(`your password confirmed`);
 }
-
 else {
     alert(`please enter a password that more 6 characters`);
 }
 
-    
+
+
+// const weight = prompt("please enter your weight(kq)");
+// const height = prompt("please enter your height(h)");
+
+// let bmi = weight/height**2;
+// alert(`Your BMI index is ${bmi}`);
 
 
 
