4b1634c63088de8c4f9f29c83cd7aedcddbd706b
nryehmdva <nurayahmadova@Nurays-MBP.bbrouter>
2023-12-07 11:59:00 +0400

    file added

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const characters = prompt(`please enter a new password`)
+
+if (password.lenght >= 6 ){ 
+
+    alert(`your password confirmed`);
+}
+
+else {
+    alert(`please enter a password that more 6 characters`);
+}
+
+    
+
+
+
+
+
